c7ca37fb9630b08b94b6c9e47eb7bb329815af29
CoderSiddharthYadav <siddhu.969488@gmail.com>
2021-03-19 07:15:54 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    
+       
+        var x=setInterval(ate,1000);
+        function ate(){
+           
+            document.getElementById('jay').innerHTML+="JAY GURU DEV"+"<br>";
+           
+        }
